4829ae6c33a765a8b63442833ae8fdd0578a81dd
Thomas Davis <thomasalwyndavis@gmail.com>
2014-10-30 21:01:14 +1000

    updated readme and api to test deploy

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 libscore
 ========
+
+
+### API Server
+
+The API server is run with node on port 3000, nginx uses a forward proxy to map it to port 80
+
+```
+cd libscore/api
+forever start -a -l /root/logs/forever.log -o /root/logs/out.log -e /root/logs/err.log api.js
+```
--- api/api.js
+++ api/api.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
+var _ = require('lodash');
+// Config variables
 
 var MONGO_URL = 'mongodb://localhost:27017/libscore';
 
@@ -12,6 +14,10 @@ MongoClient.connect(MONGO_URL, function(err, db) {
      var collection = db.collection('usage');
     // Find some documents
     collection.find({}).toArray(function(err, docs) {
+      docs = _.map(docs, function (doc) {
+        doc.blah = 'hah';
+        return docs;
+      });
       res.send(docs);
     });      
 
