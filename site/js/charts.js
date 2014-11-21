82c2974bd5bd151790afb5bc863dc4717e8f59c4
jesseDO <jesse@digitalocean.com>
2014-11-20 21:32:53 -0500

    Added in second chart
    and optimized chart js

--- site/js/charts.js
+++ site/js/charts.js
@@ -3,7 +3,8 @@
       Elements
   ***************/
 
-  var $header_logo = $("#header-logo");
+  var $header_logo = $("#header-logo"),
+  $header_title = $("#header h1");
 
   /**************
        Init
@@ -11,12 +12,14 @@
 
   $(window).load(function() {
     $.Velocity.RunSequence([
-      { elements: $header_logo, properties: "transition.fadeIn", options: { delay: 100, duration: 800} }
+      { elements: $header_logo, properties: "transition.fadeIn", options: { delay: 100, duration: 800} },
+      { elements: $header_title, properties: "transition.fadeIn", options: { delay: 250, duration: 800, sequenceQueue: false} }
+
     ]);
   });
 
 
-  var doughnutData = [
+  var topLibsData = [
   	{
   		value: 63.4,
   		color:"#29bd66",
@@ -27,7 +30,7 @@
   		value: 17.6,
   		color: "#29a478",
   		// highlight: "#5AD3D1",
-  		label: " jQuery UI"
+  		label: "jQuery UI"
   	},
   	{
   		value: 10.9,
@@ -55,25 +58,78 @@
     }
   ];
 
-  // shows legend but not tooltip
-  // window.onload = function(){
-  // 	var ctx = document.getElementById("chart-area").getContext("2d");
-  //   window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {
-  //     responsive : true,
-  //     legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-  //   });
 
-  //   document.getElementById('my-doughnut-legend').innerHTML = myDoughnut.generateLegend();
-  // };
+  var topScriptsData = [
+    {
+      value: 55.9,
+      color:"#29bd66",
+      // highlight: "#FF5A5E",
+      label: "Google Analytics"
+    },
+    {
+      value: 18.6,
+      color: "#29a478",
+      // highlight: "#5AD3D1",
+      label: "Facebook Like Button"
+    },
+    {
+      value: 12.1,
+      color: "#298c8b",
+      // highlight: "#FFC870",
+      label: "Twitter Tweet Button"
+    },
+    {
+      value: 2.7,
+      color: "#29749d",
+      // highlight: "#A8B3C5",
+      label: "Google Maps Widget"
+    },
+    {
+      value: 2.0,
+      color: "#295cb0",
+      // highlight: "#616774",
+      label: "New Relic"
+    },
+    {
+      value: 2.0,
+      color: "#2a4bbd",
+      // highlight: "#616774",
+      label: "Pinterest Pin Button"
+    },
+    {
+      value: 1.3,
+      color: "#2a4bbd",
+      // highlight: "#616774",
+      label: "Adroll"
+    },
+    {
+      value: 1.2,
+      color: "#2a4bbd",
+      // highlight: "#616774",
+      label: "TypeKit"
+    },
+    {
+      value: 1.2,
+      color: "#2a4bbd",
+      // highlight: "#616774",
+      label: "LinkedIn Share Button"
+    }
+  ];
 
   var helpers = Chart.helpers;
-  var topLib = new Chart(document.getElementById("topLib").getContext("2d")).Doughnut(doughnutData, {
+  var topLib = new Chart(document.getElementById("topLib").getContext("2d")).Doughnut(topLibsData, {
       tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>%",
       animateRotate: true,
-      animation: false,
+      animation: true,
+      animationSteps : 60,
+      animationEasing : "easeOutQuad",
+      onAnimationComplete: function(){
+        setTimeout(function(){
+          $(".legend").addClass('show');
+        }, 200);
+      },
       percentageInnerCutout : 25,
       legendTemplate : "<ul class=\"legend\" id=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%><p><%=segments[i].value%>%</p></li><%}%></ul>"
-
   });
 
   var legendHolder = document.createElement('div');
@@ -93,6 +149,41 @@
   });
 
   topLib.chart.canvas.parentNode.parentNode.appendChild(legendHolder.firstChild);
+
+
+  var helpers = Chart.helpers;
+  var topScript = new Chart(document.getElementById("topScript").getContext("2d")).Doughnut(topScriptsData, {
+      tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>%",
+      animateRotate: true,
+      animation: true,
+      animationSteps : 60,
+      animationEasing : "easeOutQuad",
+      onAnimationComplete: function(){
+        setTimeout(function(){
+          $(".legend").addClass('show');
+        }, 200);
+      },
+      percentageInnerCutout : 25,
+      legendTemplate : "<ul class=\"legend\" id=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%><p><%=segments[i].value%>%</p></li><%}%></ul>"
+  });
+
+  var legendHolder = document.createElement('div');
+  legendHolder.innerHTML = topScript.generateLegend();
+
+  helpers.each(legendHolder.firstChild.childNodes, function (legendNode, index) {
+    helpers.addEvent(legendNode, 'mouseover', function () {
+      var activeSegment = topScript.segments[index];
+      activeSegment.save();
+      topScript.showTooltip([activeSegment]);
+      activeSegment.restore();
+    });
+  });
+
+  helpers.addEvent(legendHolder.firstChild, 'mouseout', function () {
+    topScript.draw();
+  });
+
+  topScript.chart.canvas.parentNode.parentNode.appendChild(legendHolder.firstChild);
 
 
 })();
