52ad410fb26cb51afe060c1f34ecc96df5230cd5
Thomas Davis <thomasalwyndavis@gmail.com>
2014-12-17 02:56:20 +1000

    gh pages branch

---
+++ js/charts.js
@@ -0,0 +1,289 @@
+(function(){
+  /***************
+      Elements
+  ***************/
+
+  var $header_logo = $("#header-logo"),
+  $header_title = $("#header h1");
+
+  /**************
+       Init
+  **************/
+
+  $(window).load(function() {
+    $.Velocity.RunSequence([
+      { elements: $header_logo, properties: "transition.fadeIn", options: { delay: 100, duration: 800} },
+      { elements: $header_title, properties: "transition.fadeIn", options: { delay: 250, duration: 800, sequenceQueue: false} }
+    ]);
+  });
+
+  var topLibsData = {
+    labels: ["jQuery", "jQuery UI", "Modernizr", "Ajax Form", "Fancy Box", "Carousel"],
+    datasets: [
+      {
+          fillColor: "rgba(41,189,102,1)",
+          strokeColor: "rgba(220,220,220,0.8)",
+          highlightFill: "rgba(41,189,102,0.7)",
+          highlightStroke: "rgba(220,220,220,1)",
+          data: [63.4, 17.6, 10.9, 8.7, 7.9, 7.3]
+      }
+    ]
+  };
+
+  var topABData = {
+    labels: ["Optimizely", "Visual Website Optimizer"],
+    datasets: [
+      {
+          fillColor: "rgba(41,189,102,1)",
+          strokeColor: "rgba(220,220,220,0.8)",
+          highlightFill: "rgba(41,189,102,0.7)",
+          highlightStroke: "rgba(220,220,220,1)",
+          data: [.86, .59]
+      }
+    ]
+  };
+
+  var topChatData = {
+    labels: ["Zopim", "Olark", "LiveChat"],
+    datasets: [
+      {
+          fillColor: "rgba(41,189,102,1)",
+          strokeColor: "rgba(220,220,220,0.8)",
+          highlightFill: "rgba(41,189,102,0.7)",
+          highlightStroke: "rgba(220,220,220,1)",
+          data: [.94, .44, .34]
+      }
+    ]
+  };
+
+  var topCMSData = {
+    labels: ["Wordpress", "Blogger", "Shopify", "Squarespace"],
+    datasets: [
+      {
+          fillColor: "rgba(41,189,102,1)",
+          strokeColor: "rgba(220,220,220,0.8)",
+          highlightFill: "rgba(41,189,102,0.7)",
+          highlightStroke: "rgba(220,220,220,1)",
+          data: [4.2, 2.0, .28, .14]
+      }
+    ]
+  };
+
+
+  var topScriptsData = [
+    {
+      value: 55.9,
+      color:"#29bd66",
+      // highlight: "#FF5A5E",
+      label: "Google Analytics"
+    },
+    {
+      value: 18.6,
+      color: "#29a478",
+      // highlight: "#5AD3D1",
+      label: "Facebook Like Button"
+    },
+    {
+      value: 12.1,
+      color: "#298c8b",
+      // highlight: "#FFC870",
+      label: "Twitter Tweet Button"
+    },
+    {
+      value: 2.7,
+      color: "#29749d",
+      // highlight: "#A8B3C5",
+      label: "Google Maps Widget"
+    },
+    {
+      value: 2.0,
+      color: "#295cb0",
+      // highlight: "#616774",
+      label: "New Relic"
+    },
+    {
+      value: 2.0,
+      color: "#2a4bbd",
+      // highlight: "#616774",
+      label: "Pinterest Pin Button"
+    },
+    {
+      value: 1.3,
+      color: "#2a4bbd",
+      // highlight: "#616774",
+      label: "Adroll"
+    },
+    {
+      value: 1.2,
+      color: "#2a4bbd",
+      // highlight: "#616774",
+      label: "TypeKit"
+    },
+    {
+      value: 1.2,
+      color: "#2a4bbd",
+      // highlight: "#616774",
+      label: "LinkedIn Share Button"
+    }
+  ];
+
+  var topMVCData = [
+    {
+      value: .79,
+      color:"#29bd66",
+      // highlight: "#FF5A5E",
+      label: "Backbone"
+    },
+    {
+      value: .49,
+      color: "#29a478",
+      // highlight: "#5AD3D1",
+      label: "Angular"
+    },
+    {
+      value: .2,
+      color: "#295cb0",
+      // highlight: "#616774",
+      label: "Knockout"
+    },
+    {
+      value: .002,
+      color: "#298c8b",
+      // highlight: "#FFC870",
+      label: "React"
+    },
+    {
+      value: .0018,
+      color: "#29749d",
+      // highlight: "#A8B3C5",
+      label: "Ember"
+    }
+  ];
+
+  var topAnalyticsData = [
+    {
+      value: 55.9,
+      color:"#29bd66",
+      // highlight: "#FF5A5E",
+      label: "Google Analytics"
+    },
+    {
+      value: 2.2,
+      color: "#29a478",
+      // highlight: "#5AD3D1",
+      label: "Socrecard Research"
+    },
+    {
+      value: 1.9,
+      color: "#295cb0",
+      // highlight: "#616774",
+      label: "StatCounter"
+    },
+    {
+      value: 1.4,
+      color: "#298c8b",
+      // highlight: "#FFC870",
+      label: "Histats"
+    },
+    {
+      value: .75,
+      color: "#29749d",
+      // highlight: "#A8B3C5",
+      label: "Alexa"
+    },
+    {
+      value: .62,
+      color: "#29749d",
+      // highlight: "#A8B3C5",
+      label: "Clicky"
+    },
+    {
+      value: .56,
+      color: "#29749d",
+      // highlight: "#A8B3C5",
+      label: "Chartbeat"
+    },
+    {
+      value: .36,
+      color: "#29749d",
+      // highlight: "#A8B3C5",
+      label: "Mixpanel"
+    }
+  ];
+
+  var donutDefaults = {
+    tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>%",
+    animateRotate: true,
+    animation: true,
+    segmentStrokeColor : "#f2f2f2",
+    animationSteps : 60,
+    animationEasing : "easeOutQuad",
+    percentageInnerCutout : 57,
+    legendTemplate : "<ul class=\"legend\" id=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><p class=\"metric\"><%if(segments[i].label){%><%=segments[i].label%><%}%></p><p class=\"value\"><%=segments[i].value%>%</p></li><%}%></ul>"
+  }
+
+  var barDefaults = {
+    tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>%",
+    animateRotate: true,
+    animation: true,
+    scaleShowGridLines: false,
+    scaleGridLineColor : "rgba(0,0,0,.15)",
+    barShowStroke : false,
+    barStrokeWidth : 2,
+    barValueSpacing : 5,
+    barDatasetSpacing : 3,
+    animationEasing : "easeOutQuad",
+    //legendTemplate : "<ul class=\"legend\" id=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><p class=\"metric\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></p><p class=\"value\"><%=datasets[i].value%>%</p></li><%}%></ul>"
+  }
+
+  var helpers = Chart.helpers;
+  var legendHolder = document.createElement('div');
+
+  function createLegend(dataName){
+    legendHolder.innerHTML = dataName.generateLegend();
+
+    helpers.each(legendHolder.firstChild.childNodes, function (legendNode, index) {
+      helpers.addEvent(legendNode, 'mouseover', function () {
+        var activeSegment = dataName.segments[index];
+        activeSegment.save();
+        dataName.showTooltip([activeSegment]);
+        activeSegment.restore();
+      });
+
+      helpers.addEvent(legendHolder.firstChild, 'mouseout', function () {
+        dataName.draw();
+      });
+    });
+
+    dataName.chart.canvas.parentNode.parentNode.appendChild(legendHolder.firstChild);
+  }
+
+
+  // Top Libraries
+  var topLib = new Chart(document.getElementById("topLib").getContext("2d")).Bar(topLibsData, barDefaults);
+
+  // Top Scripts
+  var topScript = new Chart(document.getElementById("topScript").getContext("2d")).Doughnut(topScriptsData, donutDefaults);
+  createLegend(topScript);
+
+  // Top MVC
+  var topMVC = new Chart(document.getElementById("topMVC").getContext("2d")).Doughnut(topMVCData, donutDefaults);
+  createLegend(topMVC);
+
+  // Top Analytics
+  var topAnalytics = new Chart(document.getElementById("topAnalytics").getContext("2d")).Doughnut(topAnalyticsData, donutDefaults);
+  createLegend(topAnalytics);
+
+  // Top AB Testing
+  var topAB = new Chart(document.getElementById("topAB").getContext("2d")).Bar(topABData, barDefaults);
+
+  // Live Chat 
+  var topChat = new Chart(document.getElementById("topChat").getContext("2d")).Bar(topChatData, barDefaults);
+
+  // CMS
+  var topCMS = new Chart(document.getElementById("topCMS").getContext("2d")).Bar(topCMSData, barDefaults);
+
+  //var myBarChart = new Chart(document.getElementById("topMVC").getContext("2d")).Bar(jesse, donutDefaults);
+
+
+})();
---
+++ js/script.js
@@ -0,0 +1,463 @@
+/***************
+     Vars
+***************/
+
+var hiddenVariables = [
+	/* jQueryUI ($.ui) */
+	"$.fn.autocomplete", "$.fn.tooltip", "$.fn.tabs", "$.widget", "$.fn.button", "$.fn.mouse", "$.fn.menu", "$.position",
+	"$.fn._focus", "$.fn._show", "$.fn._hide", "$.fn._addClass", "$.fn._removeClass", "$.fn._toggleClass", "$.fn.__toggle",
+	"$.fn.selectable", "$.fn.progressbar", "$.fn.buttonset", "$.effects", "$.fn.effect", "$.fn.draggable", "$.fn.droppable",
+	"$.fn.datepicker", "$.datepicker", "$.fn.dialog", "$.fn.slider", "$.fn.sortable", "$.fn.resizable",
+	"$.fn.accordion", "$.fn.scrollParent", "$.fn.uniqueId", "$.fn.removeUniqueId", "$.fn.focus",
+	"$.fn.disableSelection", "$.fn.enableSelection", "$.fn.zIndex", "$.fn.toggleClass", "$.fn.switchClass",
+	"$.fn.cssUnit", "$.data", "$.focusable", "$.tabbable", "$.Widget", "$.fn.propAttr",
+	/* Ajax Form */
+	"$.fieldValue", "$.fn.fieldValue", "$.fn.ajaxSubmit", "$.fn.formToArray", "$.fn.ajaxFormUnbind", "$.fn.clearForm", "$.fn.formSerialize",
+	"$.fn.fieldSerialize", "$.fn.enable", "$.fn.clearInputs", "$.fn.clearFields", "$.fn.resetForm",
+	/* Google Search (GSearch) */
+	"GSearchForm", "GwebSearch", "GcustomwebSearch", "GbookSearch", "GblogSearch", "GvideoSearch", "GnewsSearch", "GlocalSearch",
+	"GcustomimageSearch", "GpatentSearch", "GSearchControl", "GsearcherOptions", "GdrawOptions", "GimageSearch",
+	/* Google Misc. */
+	"CreativeToolset", "CreativeToolsetProxy", "ExpandableAdSlotFactory", "DhtmlExpandableIframeFactory", "GPT_jstiming", "GA_jstiming", "ToolbarApi",
+	/* Keywords */
+	"DEBUG", "Analytics", "Fx", "Try", "Chain", "Selectors", "Class", "Form", "Window", "Events", "Elements", "Options", "Type", "Toggle", "Position",
+	"Browser", "Asset", "Field", "Insertion", "Color", "Request", "Tips", "Group", "Effect", "Native", "Builder", "Control", "Locale", "Table",
+	/* GSAP */
+	"Ease", "Strong", "Power4", "Quint", "Power3", "Quart", "Power2", "Cubic", "Quad", "Power1", "Linear", "Power0", "Back", "BackOut", "BackIn",
+	"BackInOut", "SlowMo", "BounceOut", "BounceIn", "SteppedEase", "BounceInOut", "Bounce", "CircOut", "CircIn", "CircInOut", "CSSPlugin", "Circ",
+	"ElasticOut", "ElasticIn", "ElasticInOut", "Elastic", "ExpoOut", "ExpoIn", "ExpoInOut", "Expo", "SineOut", "RoughEase", "SineIn", "Sine", "SineInOut", "EaseLookup",
+	/* Temporary */
+	"IframeBase", "Iframe", "IFrame", "IframeProxy", "IframeWindow"
+];
+
+/* Google misc., Drupal Webform, ClickTale, Baidu, Wordpress, Misc. */
+var hiddenVariableGroups = /^(Goog_.+|G(A|S)_google.+|WebForm_.+|WR[A-z]{1,2}|ClickTale[A-z]+|BAIDU_[A-z]+|\$(\.fn)?\.wpcf7[A-z].+|Sys\$[A-z].+|[A-Z][0-9]{1,2}|[A-z]$|[A-Z][a-z]$)/;
+
+/***************
+    Elements
+***************/
+
+var $html = $("html"),
+	$body = $("body"),
+	$header_code = $("#header-code"),
+	$header_code_object = $("#header-code-object"),
+	$header_code_property = $("#header-code-property"),
+	$header_logo = $("#header-logo"),
+	$header_logo_o = $("#header-logo o"),
+	$main = $("main"),
+	$slogan = $("#slogan"),
+	$subSlogan = $("#subSlogan"),
+	$data = $("#data"),
+	$data_table = $("#data table"),
+	$data_cols = $("#data_cols"),
+	$data_scroll = $("#data_scroll"),
+	$about = $("#about"),
+	$sectionHeader_search = $("#sectionHeader--search"),
+	$search_ = $("#search_"),
+	$search = $("#search"),
+	$searchSymbols = $("#searchSymbols"),
+	$queryButtons = $("#queryButtons span"),
+	$howTo = $("#howTo"),
+	$footer = $("footer"),
+	$subscribe_ = $("#subscribe_"),
+	$subscribe = $("#subscribe"),
+	$searchWrap = $("#searchWrap");
+	
+
+/**************
+    Global
+**************/
+
+$body.on("click", "a", function(event) {
+	if (event.target.id !== "view_trending_data") {
+		event.preventDefault();
+		window.open(event.target.href)
+	}
+});
+
+$body.on("click", "[data-query]", function(event) {
+	var target = event.target,
+		dataQuery = target.getAttribute("data-query");
+
+	if (dataQuery !== null) {
+		var query = dataQuery || $(target).text();
+
+		for (var i = 0; i < query.length; i++) {
+			(function(j) {
+				setTimeout(function() {
+					$search.val(query.slice(0, j + 1));
+
+					if (j === query.length - 1) {
+						setTimeout(function() {
+							UI.query($search[0]);
+						}, 75);
+					}
+				}, j * (18 - Math.min(12, query.length)) * 3);
+			})(i);
+		}
+	}
+});
+
+$(window).on("scroll", function() {
+	$data_scroll.hide();
+});
+
+/***************
+    Helpers
+***************/
+
+$search.one("mouseup", function() {
+	$search.select();
+});
+
+/**************
+      UI
+**************/
+
+var UI = {
+	loading: false,
+	error: function() {
+		$search.addClass('error');
+
+		$.Velocity.RunSequence([
+			{ elements: $search_, properties: "callout.shake", options: { delay: 800, duration: 450, sequenceQueue: false } }
+		]);
+	},
+	query: function() {
+		var data = $search.val();
+		
+		$("#header-logo").on("click", function (){
+			if ($("body").hasClass("results")) {
+      			window.location.hash = "";
+        		$body.removeClass("results");
+				$data_table.empty();
+				$data_table.removeClass("show");
+				$data_cols.removeClass("show");
+				$searchSymbols.removeClass("show");
+			}
+		});
+
+		function request (query, callback) {
+			var API = {
+					hostname: "http://107.170.240.125/v1/",
+					librariesPath: "libraries/",
+					sitesPath: "sites/",
+					scriptsPath: "scripts/"
+				},
+				queryNormalized;
+
+			function ajax(url) {
+				$search.removeClass('error');
+				UI.loading = true;
+				$data_table.removeClass('show');
+				$data_cols.removeClass("show");
+				$body.addClass("results");
+				$searchSymbols.addClass("show");
+				$html.css("cursor", "wait");
+				
+				//giving a lag to let the animation complete
+				setTimeout(function(){
+					$.ajax({
+						url: API.hostname + url,
+						dataType: "json",
+						complete: function() {
+							// UI.loading = false;
+							$searchSymbols.removeClass("show");
+							$html.css("cursor", "default");
+						},
+						success: function (response) {
+							if (response && response.meta) {
+
+								$(window).scrollTop(0);
+								callback(response);
+								$data_table.addClass('show');
+								$data_cols.addClass("show");
+							} else {
+								UI.error();
+							}
+						},
+						error: UI.error
+					});
+				}, 700);
+			}
+
+			$data_scroll.velocity("fadeOut");
+
+			if (/\.js$/.test(query)) {
+				alert("Be careful: We noticed you suffixed your search query with '.js'. Instead, you need to enter the exact case-sensitive VARIABLE that a library exposes itself under -- not simply the name of the library.");
+			}
+
+			query = $.trim(query.replace(/^(^https?:\/\/)?(www\.)?/i, "").replace(/^jQuery\./i, "$.").replace(/\.js$/i, ""));
+			if (query === "jquery" || query === "$") {
+				if (query === "jquery") {
+					alert("Be careful: Variable lookup is case sensitive. We've gone ahead and turned 'jquery' into 'jQuery' for you. Remember that you need to enter the exact case-sensitive VARIABLE that a library epxoses itself under -- not simply the name of the library.");
+				}
+
+				query = "jQuery";
+			}
+
+			$search.val(query);
+			// disabling this for dev
+			window.location.hash = query;
+
+			if (/^[-A-z0-9]+\.[-A-z0-9]+$/.test(query)) {
+				queryNormalized = "site";
+				query = query.toLowerCase();
+			} else if (/^script: ?[-A-z0-9]+/.test(query)) {
+				queryNormalized = "script";
+				query = query.replace(/^script: */, "").toLowerCase();
+			} else {
+				queryNormalized = query;
+			}
+
+			if (query === "libscore.com") {
+				alert("OMG. RECURSION!!@#!KJ$K BRAIN EXPLOSION. AHAHAHAHAHAHHHHHHH\n\nJust kidding. But there's no data to show right now...")
+			}
+
+			switch (queryNormalized) {
+				case "site":
+					UI.requestTarget = "site";
+					ajax(API.sitesPath + query + "/?");
+					break;
+				case "*.*":
+					UI.requestTarget = "sites";
+					ajax(API.sitesPath + "?limit=1000");
+					break;
+				case "script":
+					UI.requestTarget = "script";
+					ajax(API.scriptsPath + query + "?limit=1000");
+					break;
+				case "script:*":
+					UI.requestTarget = "scripts";
+					ajax(API.scriptsPath + "?limit=1000");
+					break;
+				case "*":
+					UI.requestTarget = "libs";
+					ajax(API.librariesPath + "?limit=1500");
+					break;
+
+				default:
+					UI.requestTarget = "lib";
+					ajax(API.librariesPath + query + "?limit=1000");
+			}
+		}
+
+		function prettifyName (name, type) {
+			var prettified = name || "";
+			var truncated = false;
+
+			if (name && name.length > 40) {
+				truncated = true;
+				prettified = prettified.slice(0, 37);
+			}
+
+			if (truncated) {
+				prettified = "<span title='" + name + "'>" + prettified + "…</span>";
+			}
+
+			if (type === "mobile") {
+				prettified += " <span title='Appears on the mobile version of this site.' class='accent'>[mobile]</span>";
+			}
+
+			return prettified;
+		}
+
+		function prettifyNumber (number, isRank) {
+			var prettified = numeral(number).format("0,0").replace(/,/g, "<span class='accent'>,</span>");
+
+			if (isRank === true) {
+				prettified = "<span class='text-grey'>#</span>" + prettified;
+			}
+
+			return prettified;
+		}
+
+		var matches;
+
+		request(data, function(response) {
+
+			var $html = "";
+			var $columns;
+
+			if (response.scripts) {
+				response.libraries = response.libraries.concat(response.scripts.map(function(obj) { obj.name = "script:" + obj.name; return obj; }));
+			}
+
+			matches = (response.results || response.sites || response.libraries);
+			matches.forEach(function(match) {
+				var $matchData;
+
+				if (hiddenVariables.indexOf((match.library || match.name)) !== -1 || hiddenVariableGroups.test((match.library || match.name))) {
+					return;
+				}
+
+				switch (UI.requestTarget) {
+					case "site":
+						$columns = "<h3 class='middle'><span>Site: </span> " + data + " </h3><div>library</div>" + data + "<div>site count</div>";
+
+						var isScript = /^script:/.test(match.name);
+
+						if (isScript) {
+							$matchData = "<td><a href='//" + match.name.replace(/^script:/, "") + "'>" + prettifyName(match.name, match.type) + "</a> <span class='text-blue'>⬈</span></td>";
+						} else {
+							$matchData = "<td><a href='//" + (match.github ? ("github.com/" + match.github) : "github.com/julianshapiro/libscore/issues/1") + "'>" + prettifyName(match.name, match.type);
+							$matchData += " <span class='text-blue'>⬈</span></a>";
+						}
+
+						$matchData += "<td>" + prettifyNumber(match.count) + "</td>";
+						break
+
+					case "sites":
+						$columns = "<h3 class='middle'><span>Top Sites</span></h3><div>site</div><div>site rank</div>";
+						$matchData = "<td><span data-query='" + match.url + "'>" + prettifyName(match.url) + "</span> <span class='text-green'>→</span></td>";
+						$matchData += "<td>" + prettifyNumber(match.rank, true) + "</td>";
+						break;
+
+					case "lib":
+						$columns = "<h3 class='middle'><span>Library: </span> " + data + " </h3><div><span id='data_badge'>" + prettifyNumber(response.count) + "</span> sites <a href='http://107.170.240.125/badge/" + $search.val() + ".svg'>Get badge</a></div></div><div>site rank</div>";
+						$matchData = "<td><a href='//" + match.url + "'>" + prettifyName(match.url) + " <span class='text-blue'>⬈</span></a></td>";
+						$matchData += "<td>" + prettifyNumber(match.rank, true) + "</td>";
+						break;
+
+					case "libs":
+						$columns = "<h3 class='middle'><span>Top Libs</span></h3><div>library <a href='http://api.libscore.com/latest/libraries.txt'>Download list</a></div><div>site count</div>";
+						$matchData = "<td><a href='http://" + (match.github ? ("github.com/" + match.github) : "github.com/julianshapiro/libscore/issues/1") + "' data-hint='Click to help track down this library.'>" + prettifyName(match.library) + "</a> <span class='text-blue'>⬈</span></a>";
+						$matchData += "<td>" + prettifyNumber(match.count) + "</td>";
+						break;
+
+					case "script":
+						$columns = "<h3 class='middle'><span>Script: </span> " + data + " </h3><div>" + prettifyNumber(response.count) + " sites</div><div>site rank</div>";
+						$matchData = "<td><a href='//" + match.url + "'>" + prettifyName(match.url) + " <span class='text-blue'>⬈</span></a></td>";
+						$matchData += "<td>" + prettifyNumber(match.rank, true) + "</td>";
+						break;
+
+					case "scripts":
+						$columns = "<h3 class='middle'><span>Top Scripts</span></h3><div>script</div><div>site count</div>";
+						$matchData = "<td><span data-query='script:" + match.script + "'>" + prettifyName(match.script) + "</span> <span class='text-green'>→</span></td>";
+						$matchData += "<td>" + prettifyNumber(match.count) + "</td>";
+						break;
+				}
+
+				$html += "<tr>" + $matchData + "</tr>";
+			});
+
+			$data_table
+				.scrollTop = 0;
+
+			$data_scroll
+				.velocity("transition.fadeIn", { delay: 1000, duration: 1000 });
+
+			if ($html) {
+				$data_cols.html($columns);
+				$data_table.html($html);
+			} else {
+				$data_cols.html("<div>library</div><div>site count</div>")
+				$data_table.html("<tr><td class='text-red'>No libraries or scripts were detected on this site.</td><td class='text-red'>Ø</td></tr>");
+			}
+
+			if (matches.length > 900) {
+				$data_table.append("<tr id='data_more'><td>Results after 1,000 are not shown. Query our <a href='https://github.com/julianshapiro/libscore'>API</a> for a complete list.</td><td></td></tr>");
+			}
+
+			setTimeout(function() {
+				$("#data").resize();
+			}, 2000);
+		});
+	}
+};
+
+/**************
+    Events
+**************/
+
+/* Input behavior. */
+$("input").on("keydown", function(event) {
+	if (event.keyCode === 13 && this.value !== "") {
+		if (event.target.id === "search" && this.value !== "variable (case sensitive) or domain...") {
+			UI.query(event.target);
+		} else if (this.id === "subscribe" && this.value) {
+			$subscribe_
+				.submit()
+				.remove();
+		}
+	}
+});
+
+/**************
+     Init
+**************/
+
+$(window).load(function() {
+	$.Velocity.RunSequence([
+		{ elements: $header_logo.add($slogan).add($search_).add($queryButtons).add($howTo).add($howTo.find("p")), properties: "transition.fadeIn", options: { stagger: 50, duration: 300 } },
+		{ elements: $header_logo_o, properties: { opacity: [ 0.6, 1 ] }, options: { sequenceQueue: false, duration: 500, loop: 2 } },
+		{ elements: $header_code, properties: "transition.fadeIn", options: { sequenceQueue: false, duration: 500
+			, begin: 
+			function() {
+					var hash = window.location.hash.slice(1);
+
+					if (hash) {
+						$search.attr("placeholder", hash);
+						$search.val(hash);
+						UI.query();
+					} else {
+						$search.attr("placeholder", "variable (case sensitive) or domain...");
+					}
+
+					[ "location()", "hash()", "map()", "<a href='//medium.com/@Shapiro/be93165fa497'>About</a>" ].forEach(function(val, i) {
+						$.Velocity($header_code_property, "transition.vanishBottomIn",
+							{ 
+								delay: i === 0 ? 125 : 0,
+								duration: 250,
+								begin: function() {
+									$header_code_property.html(val);
+
+									if (i === 1) {
+										$header_code_object.html("Array.");
+									}
+								},
+								complete: function() {
+									if (i === 3) {
+										$.Velocity.RunSequence([
+											{ elements: $header_code_object, properties: "transition.fadeOut", options: { duration: 400 } },
+											{ elements: $header_code_property, properties: "callout.pulse", options: { duration: 650 } }
+										]);
+									}
+								}
+							}
+						);
+					});
+			}
+			}
+		},
+		{ elements: $header_logo_o, properties: { opacity: 0.9, color: "#24A85A" }, options: { duration: 2000, loop: true } }
+	]);
+});
+
+function stickyNav(){
+	var scrollTop     = $(window).scrollTop(),
+  elementOffset = $('#query').offset().top + 65,
+  distance      = (elementOffset - scrollTop);
+
+	if(scrollTop > distance){
+		$("#searchWrap").addClass("sticky");
+		$("#data").addClass("sticky");
+	} else {
+		$("#searchWrap").removeClass("sticky");
+		$("#data").removeClass("sticky");
+	}
+}
+
+if ($(window).width() < 850) {
+	$(window).on('scroll', function() {
+		stickyNav();
+	});
+}
+
+$(window).resize( function(e){
+  if ($(window).width() < 850) {
+		$(window).on('scroll', function() {
+			stickyNav();
+		});
+	}
+});
