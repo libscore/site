ed503cdee3836f61102139c3f1ce2bf9a58990e4
Jason Chen <jhchen7@gmail.com>
2015-06-02 14:18:33 -0700

    Support processing multiple dump files per crawl
    This script will probably change in the next crawl but could be a useful
    reference

--- ingestion/ingest.js
+++ ingestion/ingest.js
@@ -11,26 +11,80 @@ var MONGO_URL = 'mongodb://localhost:27017/libscore';
 // REPLACE THESE VARIABLES
 var rankCounter = 0;
 
+// Used to keep aggregate totals of library usage
+var libraryUsage = {};
 
-var ingest = function (dumpFilePath, db) {
+// Used to keep aggregate totals of script usage
+var scriptUsage = {};
 
-  var crawlTime = new Date().getTime();
+var crawlTime = new Date().getTime();
+
+var sitesCache = {};
+
+MongoClient.connect(MONGO_URL, function(err, db) {
+  console.log("Connected correctly to server");
+
+  // Start the ingestion process
+  async.times(20, function(n, next){
+    ingest('dump-' + (n+1) + '.json', db, next);
+  }, function(err) {
+    // Turn it into an array for easier sorting
+    var libraryUsageArray = _.map(libraryUsage, function(value, key) {
+      var lib = {library: key, count:value, crawlTime: crawlTime};
+      return lib;
+    });
+
+    // Only include libraries that meet the threshold usages
+    libraryUsageArray = _.filter(libraryUsageArray, function(lib) {
+      return lib.count > LIBRARY_USAGE_MINIMUM ? true : false;
+    });
+
+    // Insert library usage lookup
+    var libraryUsageCollection = db.collection('libraryUsage');
+
+    libraryUsageCollection.insert(libraryUsageArray, function(err, result) {
+      console.log("Inserted Library Usage Collection", result.length);
+    });
+
+
+    // Insert script usage lookup
+    var scriptUsageCollection = db.collection('scriptUsage');
+
+    // Turn it into an array for easier sorting
+    var scriptUsageArray = _.map(scriptUsage, function(value, key) {
+      var script = {script: key, count:value, crawlTime: crawlTime};
+      return script;
+    });
+
+    // Only include libraries that meet the threshold usages
+    scriptUsageArray = _.filter(scriptUsageArray, function(script) {
+      return script.count > SCRIPT_USAGE_MINIMUM ? true : false;
+    });
+
+    scriptUsageCollection.insert(scriptUsageArray, function(err, result) {
+      console.log("Inserted Script Usage Collection", result.length);
+    });
+
+    // Log the crawl
+    var crawlsCollection = db.collection('crawls');
+
+    crawlsCollection.insert({crawlTime: crawlTime, status: 'success'}, function(err, result) {
+      console.log("Crawl Logged");
+    });
+  });
+});
+
+function ingest(dumpFilePath, db, done) {
 
   // The dump data is unfortunately not well formed json, so we have to convert it first
   utils.dumpToArray(dumpFilePath, function (crawlData){
-
-
-    // Used to keep aggregate totals of library usage
-    var libraryUsage = {};
-
-    // Used to keep aggregate totals of script usage
-    var scriptUsage = {};
 
     var sitesCollection = db.collection('sites');
 
       // 1) Calculate aggregate library usage
 
     _.each(crawlData, function(site){
+      if (sitesCache[site.url]) return;
       _.each(site.data.libs.desktop, function (lib) {
         if(typeof libraryUsage[lib] === 'undefined') {
           libraryUsage[lib] = 1;
@@ -47,19 +101,9 @@ var ingest = function (dumpFilePath, db) {
       });
     });
 
-    // Turn it into an array for easier sorting
-    var libraryUsageArray = _.map(libraryUsage, function(value, key) {
-      var lib = {library: key, count:value, crawlTime: crawlTime};
-      return lib;
-    });
-
-    // Only include libraries that meet the threshold usages
-    libraryUsageArray = _.filter(libraryUsageArray, function(lib) {
-      return lib.count > LIBRARY_USAGE_MINIMUM ? true : false;
-    });
-
     // 2) Calculate aggregate script uage
     _.each(crawlData, function(site){
+      if (sitesCache[site.url]) return;
       _.each(site.data.scripts.desktop, function (script) {
         if(typeof scriptUsage[script] === 'undefined') {
           scriptUsage[script] = 1;
@@ -77,21 +121,11 @@ var ingest = function (dumpFilePath, db) {
       });
     });
 
-    // Turn it into an array for easier sorting
-    var scriptUsageArray = _.map(scriptUsage, function(value, key) {
-      var script = {script: key, count:value, crawlTime: crawlTime};
-      return script;
-    });
-
-    // Only include libraries that meet the threshold usages
-    scriptUsageArray = _.filter(scriptUsageArray, function(script) {
-      return script.count > SCRIPT_USAGE_MINIMUM ? true : false;
-    });
-
-
     // 3 using library and script aggregate totals insert data
     //console.log('Finished', crawlData.length);
     async.eachLimit(crawlData, 20, function(site, callback){
+      if (sitesCache[site.url]) return callback();
+      sitesCache[site.url] = true;
       rankCounter++;
       var libraries = [];
       var scripts = [];
@@ -164,46 +198,7 @@ var ingest = function (dumpFilePath, db) {
       });
 
     }, function(err){
-
-      // Insert library usage lookup
-      var libraryUsageCollection = db.collection('libraryUsage');
-
-      libraryUsageCollection.insert(libraryUsageArray, function(err, result) {
-        console.log("Inserted Library Usage Collection", result.length);
-      });
-
-
-      // Insert script usage lookup
-      var scriptUsageCollection = db.collection('scriptUsage');
-
-      scriptUsageCollection.insert(scriptUsageArray, function(err, result) {
-        console.log("Inserted Script Usage Collection", result.length);
-      });
-
-      // Log the crawl
-      var crawlsCollection = db.collection('crawls');
-
-      crawlsCollection.insert({crawlTime: crawlTime, status: 'success'}, function(err, result) {
-        console.log("Crawl Logged");
-      });
-
-
-
+      done();
     });
-
-
-
-
   });
-
-
 };
-
-
-MongoClient.connect(MONGO_URL, function(err, db) {
-  console.log("Connected correctly to server");
-
-  // Start the ingestion process
-  ingest('dump.json', db);
-
-});
